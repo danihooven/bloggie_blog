38ab579db607f0c2068558589936695588ffafc3
Dani Hooven <daniellehooven@gmail.com>
2022-04-15 02:49:09 +0000

    Authentication helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
+    before_action :set_user, only: [:show, :edit, :update]
+
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 3)
     end
     
@@ -18,7 +19,8 @@ class UsersController < ApplicationController
 
         respond_to do |format|
             if @user.save
-                format.html {redirect_to articles_path, notice: 'Welcome to Bloggie Blog #{@user.username}, you have successfully signed up' }
+                session[:user_id] = @user.id
+                format.html {redirect_to articles_path, :notice => 'Welcome to Bloggie Blog ' + @user.username + ', you have successfully signed up' }
             else
                 format.html {render :new, status: :unprocessable_entity}
             end
@@ -29,13 +31,11 @@ class UsersController < ApplicationController
     
     
     def edit
-        @user = User.find(params[:id])
+        
     end
     
     
-    
     def update
-        @user = User.find(params[:id])
         respond_to do |format|
             if @user.update(user_params)
                 format.html {redirect_to user_path, notice: 'Account was updated successfully' }
@@ -51,6 +51,10 @@ class UsersController < ApplicationController
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+    
+    def set_user
+        @user = User.find(params[:id])
     end
     
 
